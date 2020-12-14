fbd2303daa58f1fd99c1be3a59d3068a03e7bf38
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-14 13:44:34 -0500

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -2,3 +2,6 @@ import React from 'react';
 import {render, screen} from "@testing-library/react";
 import MissionsList from './MissionsList';
 
+test("renders without errors", ()=>{
+    render(<MissionsList />);
+});
