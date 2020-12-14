d267f247487d89b6866283eaf6589cad6b969019
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-14 13:28:04 -0500

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {render, screen} from "@testing-library/react";
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 test('renders without errors', ()=>{
     render(<App/>);
 });
 
 test('fetches and renders mission data', async () => {
+    render(<App/>);
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
     
 });
