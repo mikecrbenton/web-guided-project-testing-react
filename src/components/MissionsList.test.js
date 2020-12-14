6211368d2bcd194d20b8313f1be453ee7abe1eb1
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-14 13:46:57 -0500

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -5,3 +5,7 @@ import MissionsList from './MissionsList';
 test("renders without errors", ()=>{
     render(<MissionsList missions={[]}/>);
 });
+
+test("renders new missions when missions data is passed", ()=>{
+
+});
